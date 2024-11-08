bda003659073527d76ac5a4df73404950a6d00f2
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-08 13:54:23 -0500

    Update reader.py

--- finchnmr/library/reader.py
+++ finchnmr/library/reader.py
@@ -4,18 +4,6 @@ Author: Nathan A. Mahynski
 
 """
 
-def bin_spectum():
-  return
-
-class Substance:
-  data: NDArray
-  extent: NDArray
-  uc0_scale: NDArray
-  uc1_scale: NDArray
-
-def __init__(self):
-  return
-
 class Bruker:
   def __init__(self):
     return
