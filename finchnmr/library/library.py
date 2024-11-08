4e6b0de2849a1fe7a247245796257a515e6a69f5
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-08 14:27:27 -0500

    Update library.py

--- finchnmr/library/library.py
+++ finchnmr/library/library.py
@@ -4,6 +4,18 @@ Author: Nathan A. Mahynski
 
 """
 
+class Substance:
+  data: NDArray
+  extent: NDArray
+  uc0_scale: NDArray
+  uc1_scale: NDArray
+
+  def __init__(self):
+    return
+
+  def resample(self):
+    return
+
 class Library:
   def __init__(self):
     return
