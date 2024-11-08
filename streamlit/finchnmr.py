618cf494b759c535b60e2314718f6299da715c3a
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-08 11:50:03 -0500

    Create finchnmr.py

---
+++ streamlit/finchnmr.py
@@ -0,0 +1,10 @@
+"""
+Interactive demonstration of FINCHnmr.
+Author: Nathan A. Mahynski
+
+"""
+import streamlit as st
+
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+st.set_page_config(layout="wide")
