252d72e79c3ac17d8523db0a0c1cd894350bcd46
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-08 13:54:10 -0500

    Create utils.py

---
+++ finchnmr/library/utils.py
@@ -0,0 +1,17 @@
+"""
+Utility functions for reading spectra.
+Author: Nathan A. Mahynski
+
+"""
+
+def bin_spectum():
+  return
+
+class Substance:
+  data: NDArray
+  extent: NDArray
+  uc0_scale: NDArray
+  uc1_scale: NDArray
+
+def __init__(self):
+  return
