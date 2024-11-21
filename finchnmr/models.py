2d90dbe1d9f1dbda6eaed44c4830064e8a8763e8
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-11-21 11:00:56 -0500

    updated models

--- finchnmr/models.py
+++ finchnmr/models.py
@@ -11,10 +11,126 @@ import numpy as np
 
 from sklearn.base import BaseEstimator, RegressorMixin
 
+from itertools import product
+
 from typing import Union, Any, Sequence
 from numpy.typing import NDArray
 
-class _Model(RegressorMixin, BaseEstimator);
+def optimize_models(
+    target_spectra: list[NDArray[np.floating]], 
+    library_spectra: NDArray[np.floating], 
+    model: "_Model",
+    param_grid: dict(str, list),
+    model_kw: Union[dict[str, Any], None] = None, 
+    binning_kw: Union[dict, None] = None
+) -> tuple[list, list]:
+    """
+    Optimize a model to fit each wild spectra in a list.
+
+    Parameters
+    ----------
+    target_spectra : list(ndarray(float, ndim=1))
+        List of flattened HSQC NMR spectra to fit.
+
+    library_spectra : ndarray(float, ndim=2)
+        Flattened HSQC NMR spectra of a library of known compounds to fit against.  Coefficients will follow the order of spectra provided here.
+
+    model : _Model
+        Uninstantiated model class to fit the spectra with.
+
+    param_grid : dict(str, list)
+        Dictionary of parameter grid to search over; this follows the same convention as `sklearn.model_selection.GridSearchCV`.
+
+    model_kw : dict(str, Any), optional(default=None)
+        Default keyword arguments to your model.
+
+    binning_kw : dict, optional(default=None)
+        Keyword arguments to `utils.bin_spectrum`.
+
+    Returns
+    -------
+    models_list : list(_Model)
+        List of optimized models fit to each target HSQC NMR spectrum.
+
+    spectra_list : list(ndarray(float, ndim=1))
+        List of HSQC NMR spectra fit during training (coarsened versions of `target_spectra`).
+
+    Example
+    -------
+    >>> models_list, spectra_list = finchnmr.models.optimize_models(
+    ...     target_spectra=target_spectra,
+    ...     library_spectra=library_spectra,
+    ...     model=finchnmr.models.LASSO,
+    ...     param_grid={'alpha':np.logspace(-5, 1, 100)},
+    ...     model_kw={'fit_intercept':False, 'positive':True},
+    ...     binning_kw=None
+    ... )
+    """
+    models_list = []
+    spectra_list = []
+        
+    if binning_kw is None:
+        binning_kw = {}
+
+    def build_fitted_model_(model_kw, param_set, library_spectra, data_to_fit):
+        if model_kw is None:
+            estimator = model() # Use model default parameters
+        else:
+            estimator = model(**model_kw) # Set basic parameters manually
+
+        estimator.set_params(param_set) # Set specific parameters (alpha, etc.)
+        _ = estimator.fit(library_spectra.T, data_to_fit.T)
+
+        return estimator
+
+    def unroll_(param_grid):
+        param_sets = []
+        for values in product(*param_grid.values()):
+            combination = dict(zip(param_grid.keys(), values))
+            param_sets.append(combination)
+
+        return param_sets
+
+    for target_spectrum in target_spectra:
+        binned_spectrum = utils.bin_spectrum(target_spectrum, **binning_kw)
+        data_to_fit = binned_spectrum.reshape(1,-1)
+
+        param_sets = unroll_(param_grid)
+        scores = []
+        for param_set_ in param_sets:
+            estimator_ = build_fitted_model_(model_kw, param_set_, library_spectra, data_to_fit)
+            scores.append(estimator_.score(library_spectra.T, data_to_fit.T))
+
+        # Fit final estimator with the "best" parameters
+        estimator = build_fitted_model_(model_kw, param_sets[np.argmax(scores)], library_spectra, data_to_fit)
+
+        models_list += [estimator]
+        spectra_list += [binned_spectrum]
+
+    return models_list, spectra_list
+
+def plot_coeffs(
+    models_list: list[Any],
+    norm: Union[str, None] = None,
+    **kwargs : Any,
+) -> None:
+    """
+    Plot the coefficients in list of LASSO models.
+
+    Parameters
+        ----------
+    models_list : list
+        List of fitted LASSO models (see `LASSO.calculate_models`).
+        
+    norm : str, optional(default=None)
+        The normalization method used to scale data to the [0, 1] range before mapping to colors.
+    """
+    coefs_list = [m.coeff() for m in models_list]
+    coefs_array = np.stack(coefs_list)
+    plt.imshow(coefs_array.T, norm=norm, **kwargs)
+    plt.colorbar()
+
+class _Model(RegressorMixin, BaseEstimator):
     """
     Model base class wrapper for linear models.
     
@@ -22,112 +138,98 @@ class _Model(RegressorMixin, BaseEstimator);
     """
     def __init__(self):
         """
+        Instantiate the model.
+
         Note that the sklearn API requires all estimators (subclasses of this) to specify all the parameters that can be set at the class level in their __init__ as explicit keyword arguments (no *args or **kwargs).
         """
         self.model_ = None
         
     def fit(self, X, y):
+        """
+        Fit the model.
+        
+        Parameters
+        ----------
+        X : ndarray(float, ndim=1)
+            Flattened library HSQC NMR spectra.
+
+        y : ndarray(float, ndim=1)
+            Flattened target HSQC NMR spectra to fit.
+
+        Returns
+        -------
+        self : _Model
+            Fitted model.
+        """
         if self.model_ is None:
             raise Exception("model has not been set yet.")
         else:
             self.__model = self.model_(**self.get_params())
+
         _ = self.__model.fit(X, y)
         self.is_fitted_ = True
+
         return self
         
     def predict(self, X):
-        return self.__model.predict(X)
-        
-    def model(self):
-        return self.__model
-
-    def coeff(self):
-        raise NotImplementedError
-        
-class LASSO(_Model):
-    def __init__(self, alpha=1.0, *, fit_intercept=True, precompute=False, copy_X=True, max_iter=1000, tol=0.0001, warm_start=False, positive=False, random_state=None, selection='cyclic'):
-        self.set_params()
-        self.model_ = sklm.Lasso
-        
-    def coeff(self):
-        return self.model().coef_
-    
-class Utils: # not needed anymore
-    """Fit LASSO model(s) from the scikit-learn library."""
-
-    @staticmethod
-    def calculate_models(
-        target_spectra: list[NDArray[np.floating]], 
-        library_spectra: NDArray[np.floating], 
-        lasso_kw: Union[dict, None] = None, 
-        binning_kw: Union[dict, None] = None
-    ) -> tuple[list, list]:
         """
-        Compute a LASSO model for a list of target spectra.
-
+        Predict the (flattened) target HSQC spectra.
+        
         Parameters
         ----------
-        target_spectra : list(ndarray(float, ndim=1))
-            List of flattened HSQC NMR spectra to fit.
-
-        library_spectra : ndarray(float, ndim=2)
-            Flattened HSQC NMR spectra of a library of known compounds to fit against.  Coefficients will follow the order of spectra provided here.
-
-        lasso_kw : dict, optional(default=None)
-            Keyword arguments to an `sklearn.linear_model.LASSO` model.
-
-        binning_kw : dict, optional(default=None)
-            Keyword arguments to `utils.bin_spectrum`.
+        X : ndarray(float, ndim=1)
+            Flattened library HSQC NMR spectra.
 
         Returns
         -------
-        models_list : list
-            List of `sklearn.linear_model.LASSO` models trained.
-
-        spectra_list : list(ndarray(float, ndim=1))
-            List of HSQC NMR spectra fit during training (coarsened versions of `target_spectra`).
+        spectrum : ndarray(float, ndim=1)
+            Predicted spectrum fit to the given library.
         """
-        models_list = []
-        spectra_list = []
+        return self.__model.predict(X)
         
-        if binning_kw is None:
-            binning_kw = {}
+    def model(self):
+        """Return the fitted model."""
+        return self.__model
+
+    def coeff(self):
+        """Return the coefficients in the model."""
+        raise NotImplementedError
         
-        if lasso_kw is None:
-            lasso_kw = {}
-
-        for target_spectrum in target_spectra:
-            binned_spectrum = utils.bin_spectrum(target_spectrum, **binning_kw)
-            data_to_fit = binned_spectrum.reshape(1,-1)
-            
-            lasso_model = sklm.Lasso(**lasso_kw)
-            lasso_model.fit(library_spectra.T, data_to_fit.T)
-            
-            models_list += [lasso_model]
-            spectra_list += [binned_spectrum]
-
-        return models_list, spectra_list
-
-    @staticmethod
-    def plot_coefs(
-        models_list: list[Any],
-        norm: Union[str, None] = None,
-        **kwargs : Any,
-    ) -> None:
+class LASSO(_Model):
+    def __init__(
+        self, 
+        alpha=1.0, 
+        *, 
+        fit_intercept=True, 
+        precompute=False, 
+        copy_X=True, 
+        max_iter=1000, 
+        tol=0.0001, 
+        warm_start=False, 
+        positive=False, 
+        random_state=None, 
+        selection='cyclic'
+    ):
         """
-        Plot the coefficients in list of LASSO models.
+        Instantiate the class.
 
-        Parameters
-        ----------
-        models_list : list
-            List of fitted LASSO models (see `LASSO.calculate_models`).
-        
-        norm : str, optional(default=None)
-            The normalization method used to scale data to the [0, 1] range before mapping to colors.
+        Inputs are identical to sklearn.linear_model.Lasso.
+        See https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html
         """
-        coefs_list = [m.coef_ for m in models_list]
-
-        coefs_array = np.stack(coefs_list)
-
-        plt.imshow(coefs_array.T, norm=norm, **kwargs)
-        plt.colorbar()
+        self.set_params(**{
+            'alpha': alpha, 
+            'fit_intercept': fit_intercept, 
+            'precompute': precompute, 
+            'copy_X': copy_X, 
+            'max_iter': max_iter, 
+            'tol': tol, 
+            'warm_start': warm_start, 
+            'positive': positive, 
+            'random_state': random_state, 
+            'selection': selection
+        })
+        self.model_ = sklm.Lasso
+        
+    def coeff(self):
+        """Return the LASSO model coefficients."""
+        return self.model().coef_
