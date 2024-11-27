667dc6f693a27e93ebe965fbe660c22bc0d1abc9
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-11-27 15:11:53 -0500

    streamlit

--- streamlit/finchnmr.py
+++ streamlit/finchnmr.py
@@ -49,3 +49,5 @@ if uploaded_file is not None:
             z.extractall('.')
             
     st.write(os.listdir('./'))
+    st.write(os.listdir('./streamlit'))
+    st.write(os.listdir('./finchnmr'))
