48b5c11956db9443ac0357a23dc9e4f3b1c895ef
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-11-27 15:26:38 -0500

    streamlit

--- streamlit/finchnmr.py
+++ streamlit/finchnmr.py
@@ -9,7 +9,7 @@ import zipfile
 
 import streamlit as st
 
-from finchnmr import substance
+from finchnmr import analysis, library, model, substance
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 st.set_page_config(layout="wide")
