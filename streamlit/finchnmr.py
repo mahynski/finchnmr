1ec7410d2c90a06469645d80259c16602eb8a025
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-11-27 15:13:21 -0500

    streamlit

--- streamlit/finchnmr.py
+++ streamlit/finchnmr.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.image("streamlit/logo_small.png")
-    st.title('FINCHnmr: [FI]tti[N]g 13[C] 1[H] HSQC NMR')
+#     st.title('FINCHnmr: [FI]tti[N]g 13[C] 1[H] HSQC NMR')
     st.markdown('''
     ## About this application    
     :heavy_check_mark: This tool is intended to demonstrate the use of [finchnmr](https://github.com/mahynski/finchnmr) to characterize the composition of mixture of compounds.
@@ -50,4 +50,4 @@ if uploaded_file is not None:
             
     st.write(os.listdir('./'))
     st.write(os.listdir('./streamlit'))
-    st.write(os.listdir('./finchnmr'))
+    st.write(os.listdir('./docs'))
