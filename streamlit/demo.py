ab1f28af3a229ecde139bb9ee10339d29c385822
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-11-27 16:14:08 -0500

    st update

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -61,7 +61,7 @@ if uploaded_file is not None:
     else:
         head = head[0]
         
-    st.write(os.listdir('./'))
+    st.write(os.listdir('./uploaded_nmr/'))
 
     # Create substance
     target = finchnmr.substance.Substance(
