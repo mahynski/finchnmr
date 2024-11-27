33b8fc86808625b9fde9d0ae0a1ae96caeeef00b
Nathan Mahynski <nathan.mahynski@gmail.com>
2024-11-27 15:08:38 -0500

    streamlit

--- streamlit/finchnmr.py
+++ streamlit/finchnmr.py
@@ -12,8 +12,7 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 st.set_page_config(layout="wide")
 
 with st.sidebar:
-    st.image("logo_small.png")
-    
+#     st.image("logo_small.png")
     st.title('FINCHnmr: [FI]tti[N]g 13[C] 1[H] HSQC NMR')
     st.markdown('''
     ## About this application    
